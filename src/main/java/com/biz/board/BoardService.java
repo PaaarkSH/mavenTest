78321c1bb174e7748a7182fe22ada7b1e6614f45
psh950 <psh950@naver.com>
2024-04-07 23:46:00 +0900

    스프링 연동1

--- src/main/java/com/biz/board/BoardService.java
+++ src/main/java/com/biz/board/BoardService.java
@@ -3,7 +3,7 @@ package com.biz.board;
 import java.util.List;
 
 public interface BoardService {
-    public void insertBoard(BoardVO vo);
+     public void insertBoard(BoardVO vo);
     public void deleteBoard(BoardVO vo);
     public void updateBoard(BoardVO vo);
     public BoardVO getBoard(BoardVO vo);
